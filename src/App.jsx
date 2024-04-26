6fb6171b086e2bece8681c04d94cb09bb07675aa
Teresa <mtmaisterra@gmail.com>
2024-04-26 12:10:54 -0300

    commitiando II

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+/* import Footer from './components/footer' */
+import NavBar from './components/navBar'
 
 function App() {
   return (
     <>
-      <div className="bg-grisOscuro w-full">        
-      <p className="bg-morado w-full px-5 text-white">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="bg-grisOscuro flex w-full flex items-center h-10">
+        <div className="bg-morado w-4/5 h-20 rounded-xl">
+          <h1 className= "w-full justify-center text-2xl px-5 text-white">
+            No le entiendo nada
+          </h1>
+        </div>
+      </div>
+      <div className='flex flex-col'>
+        <NavBar />
       </div>
     </>
   )
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,18 +1,25 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function NavBar() {
+    const history = useHistory();
+
+    const handleButtonClick = () => {
+        history.push('/Ingresar');
+    }
     return (
-        <div className='flex w-full h-10 justify-around  bg-grisClaro'>
-            <img src='./assets/MaisterraDigitalDesings.jpeg' className="w-5 h-5" />
+        <div className='h-10 justify-around  bg-morado'>
+
             <div className='w-full justify-center items-center mx-5'>
-                <button href="./registrar.jsx" className="bg-morado w-20 h-10">                    
-                       <a className='w-1/2 h-4 p-10 mx-4 text-white bg-morado text-center text-xl'>Registrar</a>                     
-                </button>
-                <button href='/Ingresar' className='mx-10 p-5 '>
-                    <p className='w-1/2 h-4 p-2 mx-4 text-white text-center text-xl bg-morado'>
-                        Ingresar
-                    </p>
-                </button>
+                {/* ="./registrar" className='w-1/2 text-current no-underline h-4 p-10 mx-4 text-white bg-morado text-center border-2 rounded-xl text-xl'>
+                        registrar */}
             </div>
+            <button onClick={handleButtonClick} className='mx-10 p-5 '>
+                <p className='w-1/2 h-4 p-2 mx-4 text-white text-center text-xl bg-morado'>
+                    Ingresar
+                </p>
+            </button>
+
         </div>
     )
 }
